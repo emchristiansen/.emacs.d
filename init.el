53bdce779206b42523582331f505f6b76a759b16
Eric Christiansen <ericmartinchristiansen@gmail.com>
2015-02-03 13:34:25 -0800

    put external-calling code in a separate script

--- init.el
+++ init.el
@@ -1,34 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Set up el-get, which should have been git cloned into the indicated
-;; directory.
+;; Configure the packages.
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-(setq el-get-verbose t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Install the desired packages.
-
-(el-get-bundle evil)
-(el-get-bundle yasnippet)
-
-(el-get-bundle scala-mode2)
-(el-get-bundle sbt-mode)
-(el-get-bundle ensime)
-
-(el-get-bundle python-mode)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Configure the packages.
 
 (require 'evil)
 (evil-mode 1)
